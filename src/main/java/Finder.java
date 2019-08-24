be69c31b4e23d597ece535957814c4f75e9bf480
Tristan Desktop <tscott96@live.com>
2019-08-23 19:33:08 -0500

    +Finder Class: implememnts findMax and findMin methods that find the max and min of an array respectively. Added JUnit tests for findMax and findMin.

---
+++ src/main/java/Finder.java
@@ -0,0 +1,30 @@
+public class Finder {
+    public static Integer findMax(int[] intArray)
+    {
+        if (intArray == null || intArray.length == 0)
+        {
+            return null;
+        }
+        int max = intArray[0];
+        for (int i = 0; i < intArray.length; i++) {
+            if (intArray[i] > max){
+                max = intArray[i];
+            }
+        }
+        return max;
+    }
+    public static Integer findMin(int[] intArray)
+    {
+        if (intArray == null || intArray.length == 0)
+        {
+            return null;
+        }
+        int min = intArray[0];
+        for (int i = 0; i < intArray.length; i++) {
+            if (intArray[i] < min){
+                min = intArray[i];
+            }
+        }
+        return min;
+    }
+}
--- src/main/java/JUnitTest.java
+++ src/main/java/JUnitTest.java
@@ -1,2 +1,34 @@
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
 public class JUnitTest {
+    @Test
+    public void test_maxInt()
+    {
+        int[] intArray = new int[]{1, 17, 89, 120, 15, 23};
+        int[] emptyArray = new int[0];
+        int[] nullArray = null;
+        Integer expectedResult = 120;
+        Integer actualResult = Finder.findMax(intArray);
+        assertEquals(expectedResult, actualResult);
+        expectedResult = null;
+        actualResult = Finder.findMax(emptyArray);
+        assertEquals(expectedResult, actualResult);
+        actualResult = Finder.findMax(nullArray);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void test_minInt()
+    {
+        int[] intArray = new int[]{1, 17, 89, 120, 15, 23};
+        int[] emptyArray = new int[0];
+        int[] nullArray = null;
+        Integer expectedResult = 1;
+        Integer actualResult = Finder.findMin(intArray);
+        assertEquals(expectedResult, actualResult);
+        expectedResult = null;
+        actualResult = Finder.findMax(emptyArray);
+        assertEquals(expectedResult, actualResult);
+        actualResult = Finder.findMax(nullArray);
+        assertEquals(expectedResult, actualResult);
+    }
 }
